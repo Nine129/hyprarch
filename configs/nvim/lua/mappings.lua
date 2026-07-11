7042aea57e6b3c59338d41cf648d5992dc6c861f
NineOnArch <vuaquyhoa@gmail.com>
2026-07-11 18:09:09 +0700

    hihi

--- configs/nvim/lua/mappings.lua
+++ configs/nvim/lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Override NvChad's <leader>cm to use file's directory (avoids "not a git directory" error)
+map("n", "<leader>cm", function()
+  require("telescope.builtin").git_commits({ cwd = vim.fn.expand("%:p:h") })
+end, { desc = "telescope git commits (from file dir)" })
