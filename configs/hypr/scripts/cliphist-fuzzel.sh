286e2ebaf8d4cbe18a8f7401280b601315790c7c
NineOnArch <vuaquyhoa@gmail.com>
2026-07-12 22:45:10 +0700

    hihi

--- configs/hypr/scripts/cliphist-fuzzel.sh
+++ configs/hypr/scripts/cliphist-fuzzel.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 selected=$(cliphist list | fuzzel --dmenu)
 [ -z "$selected" ] && exit
-decoded=$(echo "$selected" | cliphist decode)
-printf '%s' "$decoded" | wl-copy
+echo "$selected" | cliphist decode | wl-copy
 sleep 0.3
 class=$(hyprctl activewindow -j | jq -r ".class" | tr "[:upper:]" "[:lower:]")
 if [[ "$class" == "kitty" ]]; then
